fa4bca575dbb4756876ddab133f83e35cbb3d072
Ryan Febry <66952311+ryanf27@users.noreply.github.com>
2025-09-03 10:44:27 +0700

    Add database migrations

---
+++ changepreneurship-backend/migrations/env.py
@@ -0,0 +1,52 @@
+from __future__ import with_statement
+
+import logging
+from logging.config import fileConfig
+
+from flask import current_app
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = current_app.extensions['migrate'].db.metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+
+    url = current_app.config.get('SQLALCHEMY_DATABASE_URI')
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+
+    connectable = current_app.extensions['migrate'].db.engine
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
+
